258b0105095edfc94f07f027d81f977f06f2bf88
Tapan Shah <tapan.shah287@gmail.com>
2025-12-28 11:05:49 +0530

    Pycache removal

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# --- OPTIONAL: Force remove pycache ---
+find . -type d -name "__pycache__" -exec rm -r {} +
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
