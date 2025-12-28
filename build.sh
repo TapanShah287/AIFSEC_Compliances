d393729d5eafa76dd1d72cbfedacdae4f22857e7
Tapan Shah <tapan.shah287@gmail.com>
2025-12-28 11:11:26 +0530

    Database deletion updated in build

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+echo "💣 Wiping Database..."
+python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('DROP SCHEMA public CASCADE; CREATE SCHEMA public;');"
+echo "💣 Database wiped."
+
 # --- OPTIONAL: Force remove pycache ---
 find . -type d -name "__pycache__" -exec rm -r {} +
 
